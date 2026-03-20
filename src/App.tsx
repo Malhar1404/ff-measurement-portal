f6e96ba076b03a8fee9f53e2fbaa5942cccc9a10
Malhar1404 <malhar@hexacoder.com>
2026-03-20 18:09:00 +0530

    Create App Template

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Canvas3d } from './components/3d/Canvas'
+
+function App() {
+
+  return (
+    <>
+     <div className='h-screen w-screen'>
+        <Canvas3d />
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/3d/Canvas.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+export const Canvas3d = ()=>{
+    return (
+        <Canvas>
+            <Box/>
+            
+        </Canvas>
+    )
+}
